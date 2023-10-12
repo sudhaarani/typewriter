c2a50fbf8fbb0863b5a247507de425a340c5fc6a
dhaaranisuriya2605@gmail.com <dhaaranisuriya2605@gmail.com>
2023-10-12 12:07:40 -0700

    to animate the sentence, revealing one character at a time. This would make it look as it is being typed in by someone

--- typewriter.js
+++ typewriter.js
@@ -2,10 +2,10 @@ const sentence = "hello there from lighthouse labs";
 let delay = 50;
 for (const char of sentence) {
   setTimeout(() => {
-    process.stdout.write(char);
+    process.stdout.write(char); //process.stdout.write --> this prints the characters out on a single line
   }, delay);
   delay += 50;
 }
 setTimeout(() => {
-  process.stdout.write("\n");
+  process.stdout.write("\n"); // the next prompt appears on the same line, to avoid that we are delaying \n as well, so used setTimeout
 }, delay);
